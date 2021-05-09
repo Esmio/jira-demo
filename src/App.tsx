6536cac128621904eeafc914651be68683377164
simon <hy44jt@126.com>
2021-05-10 01:42:25 +0800

    ci: add commitlint

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import "./App.css";
 
 
 
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App"><header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
