df167a6dbc7ce076a2d1cb2eca436633996cc6ed
simon <hy44jt@126.com>
2021-06-23 00:12:53 +0800

    feat: 添加登录注册

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if (req.method === 'POST' && req.path == '/login') {
+  if (req.method === 'POST' && req.path === '/login') {
     if (req.body.username === 'jack' && req.body.password === '111111') {
       return res.status(200).json({
         user: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import LoginScreen from 'screens/login';
-// import ProjectListScreen from 'screens/project-list';
+
+import { useAuth } from 'context/auth-context';
+import AuthenticatedApp from 'authenticated-app';
+import { UnauthenticatedApp } from 'unauthenticated-app';
 
 import "./App.css";
 
 function App() {
+  const { user } = useAuth();
+
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
